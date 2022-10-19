38f40426a37cb595a9c4cbcd839de34d1d0c96ae
SajidSarwar12 <sajidsrwr@gmail.com>
2022-10-19 09:07:10 +0200

    routes cleaned

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Sidenav from './components/sidenav/sidenav';
 import Footer from './components/footer/footer';
 import Home from './container/home/home';
 import Login from './container/login/login';
 import Addadmin from './container/addadmin/addadmin';
 import Update from './container/update/update';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 
 
 const RoutesPage = () => {
